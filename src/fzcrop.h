25f2e137b2c1f91deaffc78100cc77f756f57305
hswhsy <hswhsy@snu.ac.kr>
2021-07-15 18:54:11 +0900

    210715

--- src/fzcrop.h
+++ src/fzcrop.h
@@ -1,5 +1,5 @@
 #ifndef FZCROP_H
 #define FZCROP_H
-List ecocropK(NumericVector inparams, NumericVector Tmax, NumericVector Tmin, 
+List fzycrop(NumericVector inparams, NumericVector Tmax, NumericVector Tmin, 
                NumericVector Prcp, LogicalVector rainfed);
 #endif
